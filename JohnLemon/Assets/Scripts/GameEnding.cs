bd10a27eb0f6f849ee79b3c8757da01e96f88bd6
c-cortex <cortes.d.chris@gmail.com>
2022-04-11 20:54:06 -0700

    Added gargoyle and ghost functionality

--- JohnLemon/Assets/Scripts/GameEnding.cs
+++ JohnLemon/Assets/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,33 +9,52 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
 
     void OnTriggerEnter (Collider other)
     {
-        if(other.gameObject == player)
+        if (other.gameObject == player)
         {
             m_IsPlayerAtExit = true;
         }
     }
 
-    void Update()
+    public void CaughtPlayer ()
     {
-        if(m_IsPlayerAtExit)
+        m_IsPlayerCaught = true;
+    }
+
+    void Update ()
+    {
+        if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel()
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-        if(m_Timer > (fadeDuration + displayImageDuration))
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                Application.Quit ();
+            }
         }
     }
 }
